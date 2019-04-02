3e566c27fd6bebbfd4bfeff7bf448f69e8566e6a
李博文 <37969663+rj172lbw@users.noreply.github.com>
2019-04-02 21:58:02 +0800

    小肥猫的工程

---
+++ 201708044216LBW/4.2_annotation/TestObject.java
@@ -0,0 +1,15 @@
+package com.atguigu.spring.beans.annotation;
+
+/**
+ * @Auther: LBW
+ * @Date: 2019/4/2
+ * @Description: com.atguigu.spring.beans.annotation
+ * @version: 1.0
+ */
+import org.springframework.stereotype.Component;
+
+@Component("testObject")  //相当于在bean中配置id
+public class TestObject
+{
+
+}
---
+++ 201708044216LBW/4.2_annotation/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.atguigu.spring.beans.annotation.repository;
+
+/**
+ * @Auther: LBW
+ * @Date: 2019/4/2
+ * @Description: com.atguigu.spring.beans.annotation.repository
+ * @version: 1.0
+ */
+public interface UserRepository
+{
+     void save();
+}
