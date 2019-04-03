8f393e7d488725ef89654e8fc4eebd9a58091921
张成文 <47981300+1582691494@users.noreply.github.com>
2019-04-03 21:00:08 +0800

    JDBC

--- 201708044206张成文/JDBC_Test/src/demo/test.java
+++ 201708044206张成文/JDBC_Test/src/demo/test.java
@@ -1,52 +1,64 @@
 package demo;
 
-import java.sql.*;
-public class test {
-
-    //执行创建表的DDl语句
-    static String creatsql = "CREATE TABLE pepole("
-            + "name varchar(10) not null,"
-            + "age int(4) not null"
-            + ")charset=utf8;";
 
     final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    //指定连接数据库的url
-    final static String DB_URL = "jdbc:mysql://localhost";
-    //mysql用户名
-    final static String name = "root";
-    //mysql密码
-    final static String pwd = "z123";
-    public static void main(String[] args)
-    {
+
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class test {
+    public static void main(String[] args) throws Exception {
         Connection conn = null;
-        Statement stmt = null;
-        try
-        {
-            //注册jdbc驱动
-            Class.forName(JDBC_DRIVER);
-            //打开连接
-            System.out.println("//连接数据库");
-            conn = DriverManager.getConnection(DB_URL,name,pwd);
-            //执行创建表
-            System.out.println("//创建表");
-            stmt = conn.createStatement();
-            if(0 == stmt.executeLargeUpdate(creatsql))
-            {
-                System.out.println("成功创建表！");
+        String sql;
+        // MySQL的JDBC URL编写方式：jdbc:mysql://主机名称：连接端口/数据库的名称?参数=值
+        // 避免中文乱码要指定useUnicode和characterEncoding
+        // 执行数据库操作之前要在数据库管理系统上创建一个数据库，名字自己定，
+        // 下面语句之前就要先创建javademo数据库
+        String url = "jdbc:mysql://202.196.37.168:3306/test?user=student&password=Student_123456&characterEncoding=UTF8&useSSL=false";
+        //这里在URL最后加入&useSSL=false就不会报错
+        try {
+            // 之所以要使用下面这条语句，是因为要使用MySQL的驱动，所以我们要把它驱动起来，
+            // 可以通过Class.forName把它加载进去，也可以通过初始化来驱动起来，下面三种形式都可以
+            Class.forName("com.mysql.jdbc.Driver");// 动态加载mysql驱动
+            // or:
+            // com.mysql.jdbc.Driver driver = new com.mysql.jdbc.Driver();
+            // or：
+            // new com.mysql.jdbc.Driver();
+
+            System.out.println("成功加载MySQL驱动程序");
+            // 一个Connection代表一个数据库连接
+            conn = DriverManager.getConnection(url);
+            // Statement里面带有很多方法，比如executeUpdate可以实现插入，更新和删除等
+            Statement stmt = conn.createStatement();
+            sql = "create table Student(NO char(20),name varchar(20),primary key(NO))";
+            int result = stmt.executeUpdate(sql);// executeUpdate语句会返回一个受影响的行数，如果返回-1就没有成功
+            if (result != -1) {
+                System.out.println("创建数据表成功");
+                sql = "insert into Student(NO,name) values('20','陶')";
+                result = stmt.executeUpdate(sql);
+                sql = "insert into Student(NO,name) values('2','在')";
+                result = stmt.executeUpdate(sql);
+                sql = "select * from Student";
+                ResultSet rs = stmt.executeQuery(sql);// executeQuery会返回结果的集合，否则返回空值
+                System.out.println("学号\t姓名");
+                while (rs.next()) {
+                    System.out.println(rs.getString(1) + "\t" + rs.getString(2));// 入如果返回的是int类型可以用getInt()
+                }
             }
-            else
-            {
-                System.out.println("创建表失败！");
-            }
-            //
-            stmt.close();
-            conn.close();
-            System.out.println("//关闭资源");
-        }
-        catch(Exception e)
-        {
-            System.out.println("创建表失败！");
+        } catch (SQLException e) {
+            System.out.println("MySQL操作错误");
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.close();
         }
+
     }
+
 }
