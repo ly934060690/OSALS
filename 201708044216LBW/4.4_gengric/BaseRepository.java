1a6c5f32101ce4a848217d5c8956bc9840b13daf
李博文 <37969663+rj172lbw@users.noreply.github.com>
2019-04-04 21:24:23 +0800

    4.4_gengric

---
+++ 201708044216LBW/4.4_gengric/BaseRepository.java
@@ -0,0 +1,12 @@
+package com.atguigu.spring.beans.gengric.di;
+
+/**
+ * @Auther: LBW
+ * @Date: 2019/4/4
+ * @Description: com.atguigu.spring.beans.gengric.di
+ * @version: 1.0
+ */
+public class BaseRepository<T>
+{
+
+}
---
+++ 201708044216LBW/4.4_gengric/UserService.java
@@ -0,0 +1,16 @@
+package com.atguigu.spring.beans.gengric.di;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Auther: LBW
+ * @Date: 2019/4/4
+ * @Description: com.atguigu.spring.beans.gengric.di
+ * @version: 1.0
+ */
+
+@Service("userService")
+public class UserService extends BaseService<User>
+{
+
+}
