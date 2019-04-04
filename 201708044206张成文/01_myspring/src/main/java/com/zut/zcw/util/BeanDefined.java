961412e507b32b226d9ed29aac482220484a3ec1
张成文 <47981300+1582691494@users.noreply.github.com>
2019-04-04 19:32:49 +0800

    spring/bean/factory

---
+++ 201708044206张成文/01_myspring/src/main/java/com/zut/zcw/util/BeanDefined.java
@@ -0,0 +1,20 @@
+package com.zut.zcw.util;
+
+public class BeanDefined {
+
+	private String beanId;
+	private String classPath;
+	public String getBeanId() {
+		return beanId;
+	}
+	public void setBeanId(String beanId) {
+		this.beanId = beanId;
+	}
+	public String getClassPath() {
+		return classPath;
+	}
+	public void setClassPath(String classPath) {
+		this.classPath = classPath;
+	}
+	
+}
