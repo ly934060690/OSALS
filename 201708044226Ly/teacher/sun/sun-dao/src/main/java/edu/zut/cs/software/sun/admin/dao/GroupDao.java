102067a887299e68b2e264ee33516a20cb102697
Mr.Ly <934060690@qq.com>
2019-04-15 22:56:29 +0800

    base dao service 课堂跟进练习  2019-4-15 22:55:46

---
+++ 201708044226Ly/teacher/sun/sun-dao/src/main/java/edu/zut/cs/software/sun/admin/dao/GroupDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.software.sun.admin.dao;
+
+import edu.zut.cs.software.base.dao.GenericTreeDao;
+import edu.zut.cs.software.sun.admin.domain.Group;
+
+public interface GroupDao extends GenericTreeDao<Group, Long> {
+
+}
