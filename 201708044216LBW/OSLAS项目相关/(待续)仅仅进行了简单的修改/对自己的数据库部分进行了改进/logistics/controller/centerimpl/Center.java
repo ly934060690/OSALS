04c4bfbbd6bce8d45f9a1bd0a5dbefc039269ce1
李博文 <37969663+rj172lbw@users.noreply.github.com>
2019-04-21 20:42:55 +0800

    使用JDBCTemplate方法对自己数据库访问进行了优化

---
+++ 201708044216LBW/OSLAS项目相关/(待续)仅仅进行了简单的修改/对自己的数据库部分进行了改进/logistics/controller/centerimpl/Center.java
@@ -0,0 +1,23 @@
+package edu.zut.cs.lbw.logistics.controller.centerimpl;
+
+
+import edu.zut.cs.lbw.logistics.controller.center;
+
+/**
+ * @Auther: LBW
+ * @Date: 2019/4/3
+ * @Description: com.logistics.lbw.mian
+ * @version: 1.0
+ */
+public class Center implements center
+{
+    public static void main(String []args)
+    {
+        //ApplicationContext app = new ClassPathXmlApplicationContext("Beans.xml");
+
+        //EntrePotImpl person = (EntrePotImpl) app.getBean("datasource");
+
+       //System.out.println(person.toString());     //这里输出仓库的管理人员以及种类以及货物
+    }
+
+}
---
+++ 201708044216LBW/OSLAS项目相关/(待续)仅仅进行了简单的修改/对自己的数据库部分进行了改进/logistics/repository/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @Auther: LBW
+ * @Date: 2019/4/10
+ * @Description: com.logistics.lbw.mian.repository
+ * @version: 1.0
+ */
+package edu.zut.cs.lbw.logistics.repository;
+
+//暂时未写入数据
