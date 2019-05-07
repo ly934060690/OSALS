4ff23b07214f03fdeb5f6f5588f9a790d2e026d8
张成文 <47981300+1582691494@users.noreply.github.com>
2019-05-07 17:18:10 +0800

    Spring属性配置

---
+++ 201708044206张成文/spring-1/src/com/zcw/spring/beans/collections/NewPerson.java
@@ -0,0 +1,42 @@
+package com.zcw.spring.beans.collections;
+
+import java.util.Map;
+
+import com.zcw.spring.beans.Car;
+
+public class NewPerson {
+
+	private String name;
+	private int age;
+	private Map<String, Car> cars;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Map<String, Car> getCars() {
+		return cars;
+	}
+
+	public void setCars(Map<String, Car> cars) {
+		this.cars = cars;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", cars=" + cars + "]";
+	}
+
+}
