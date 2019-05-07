4078a0d0412eb685119dbe4fbeda35deca517fb1
Mr.ju <3350815841@qq.com>
2019-05-07 20:24:52 +0800

    update

---
+++ 201708044207XPJ/OSALS项目/OSALS 5-7/OSALS-dao/src/main/java/edu/zut/cs/OSALS/admin/dao/ShopDao.java
@@ -0,0 +1,7 @@
+package edu.zut.cs.OSALS.admin.dao;
+
+import edu.zut.cs.OSALS.admin.domain.Shop;
+import edu.zut.cs.OSALS.base.dao.GenericTreeDao;
+
+public interface ShopDao extends GenericTreeDao<Shop, Long> {
+}
