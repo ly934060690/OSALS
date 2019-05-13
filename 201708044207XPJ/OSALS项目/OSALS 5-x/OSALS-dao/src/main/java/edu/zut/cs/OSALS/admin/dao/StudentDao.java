d2a1f2b585d039b4181569f4c5f0170fc87fe6ed
Mr.ju <3350815841@qq.com>
2019-05-14 00:04:26 +0800

    update

---
+++ 201708044207XPJ/OSALS项目/OSALS 5-x/OSALS-dao/src/main/java/edu/zut/cs/OSALS/admin/dao/StudentDao.java
@@ -0,0 +1,7 @@
+package edu.zut.cs.OSALS.admin.dao;
+
+import edu.zut.cs.OSALS.admin.domain.Student;
+import edu.zut.cs.OSALS.base.dao.GenericDao;
+
+public interface StudentDao extends GenericDao<Student, Long> {
+}
