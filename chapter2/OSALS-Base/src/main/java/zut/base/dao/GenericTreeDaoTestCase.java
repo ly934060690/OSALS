2caafc8c38660f24ba70890a62b972a9accbc27b
Mr.Ly <934060690@qq.com>
2019-05-14 17:28:14 +0800

    Delete  2019-5-14 17:28:07

--- chapter2/OSALS-Base/src/main/java/zut/base/dao/GenericTreeDaoTestCase.java
+++
@@ -1,9 +0,0 @@
-package zut.base.dao;
-
-import zut.base.domain.BaseTreeEntity;
-
-import java.io.Serializable;
-
-public class GenericTreeDaoTestCase<PK extends Serializable, T extends BaseTreeEntity<T>, M extends GenericTreeDao<T, PK>>
-        extends GenericDaoTestCase<PK, T, M> {
-}
