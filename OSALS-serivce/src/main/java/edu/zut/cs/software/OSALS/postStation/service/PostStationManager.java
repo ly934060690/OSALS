35aec638962b8409d297d4ee36c780d608eaeb9c
Mr.Ly <934060690@qq.com>
2019-05-18 14:42:21 +0800

    Delete  2019-5-18 14:42:18

--- OSALS-serivce/src/main/java/edu/zut/cs/software/OSALS/postStation/service/PostStationManager.java
+++
@@ -1,7 +0,0 @@
-package edu.zut.cs.software.OSALS.postStation.service;
-
-import edu.zut.cs.software.OSALS.postStation.domain.PostStation;
-import edu.zut.cs.software.base.service.GenericTreeManager;
-
-public interface PostStationManager extends GenericTreeManager<PostStation, Long> {
-}
