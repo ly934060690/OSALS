738dca728b731065866d23e0e8fa4859a0be7474
李博文 <37969663+rj172lbw@users.noreply.github.com>
2019-06-19 15:47:56 +0800

    Update NlpKeywordExtraction.java

--- OSALS-dao/src/main/java/edu/zut/cs/software/osals/nlp/domain/NlpKeywordExtraction.java
+++ OSALS-dao/src/main/java/edu/zut/cs/software/osals/nlp/domain/NlpKeywordExtraction.java
@@ -20,14 +20,65 @@ import java.util.List;
 @Proxy(lazy = false)
 public class NlpKeywordExtraction extends BaseEntity {
 
-    @Column(name = "content")
-    String  content;
+    private static final long serialVersionUID = -2748027099895603067L;
 
-    public String getContent() {
-        return content;
+    @Column(name = "text")
+    String  text;
+
+    @Column(name = "text_1")
+    String word_1;
+
+    @Column(name = "text_2")
+    String word_2;
+
+    @Column(name = "text_3")
+    String word_3;
+
+    @Column(name = "text_4")
+    String word_4;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public String getText() {
+        return text;
     }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getWord_1() {
+        return word_1;
+    }
+
+    public void setWord_1(String word_1) {
+        this.word_1 = word_1;
+    }
+
+    public String getWord_2() {
+        return word_2;
+    }
+
+    public void setWord_2(String word_2) {
+        this.word_2 = word_2;
+    }
+
+    public String getWord_3() {
+        return word_3;
+    }
+
+    public void setWord_3(String word_3) {
+        this.word_3 = word_3;
+    }
+
+    public String getWord_4() {
+        return word_4;
+    }
+
+    public void setWord_4(String word_4) {
+        this.word_4 = word_4;
+    }
+
 }
