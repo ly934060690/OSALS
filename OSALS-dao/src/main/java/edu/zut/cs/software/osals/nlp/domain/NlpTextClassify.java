22f4705677984443043f18ea4a429290838fb140
张成文 <47981300+1582691494@users.noreply.github.com>
2019-06-22 12:10:33 +0800

    6/22

--- OSALS-dao/src/main/java/edu/zut/cs/software/osals/nlp/domain/NlpTextClassify.java
+++ OSALS-dao/src/main/java/edu/zut/cs/software/osals/nlp/domain/NlpTextClassify.java
@@ -12,6 +12,48 @@ import javax.persistence.Table;
 public class NlpTextClassify extends BaseEntity {
     @Column(name = "TEXT")
     String text;
+    @Column(name = "CLASSIFY1")
+    String classify1;
+
+    @Column(name = "CLASSIFY2")
+    String classify2;
+    @Column(name = "CLASSIFY3")
+    String classify3;
+    @Column(name = "CLASSIFY4")
+    String classify4;
+
+    public String getClassify4() {
+        return classify4;
+    }
+
+    public void setClassify4(String classify4) {
+        this.classify4 = classify4;
+    }
+
+    public String getClassify3() {
+        return classify3;
+    }
+
+    public void setClassify3(String classify3) {
+        this.classify3 = classify3;
+    }
+
+    public String getClassify1() {
+        return classify1;
+    }
+
+    public void setClassify1(String classify1) {
+        this.classify1 = classify1;
+    }
+
+    public String getClassify2() {
+        return classify2;
+    }
+
+    public void setClassify2(String classify2) {
+        this.classify2 = classify2;
+    }
+
     public String getText() {
             return text;
         }
