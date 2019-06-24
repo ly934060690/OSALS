c743a4e05cadd31f9977a121d6bb0d1355204616
张成文 <47981300+1582691494@users.noreply.github.com>
2019-06-24 23:58:29 +0800

    以后不要动这个地方的端口号

--- nlp/config/index.js
+++ nlp/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       "/api":{
-        target:"http://localhost:8080/osals",
+        target:"http://localhost:8081/osals",
         //change HOST
         changeOrigin:true,
         pathRewrite:{
